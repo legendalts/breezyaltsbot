3b8a60d9adb5b64edb5d8176d0e59291a2315650
legendalts <35744444+legendalts@users.noreply.github.com>
2018-01-29 13:54:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,14 @@ client.on('message', msg => {
 	    		msg.channel.send("All the alts are sent to the DMs.");
 			msg.author.send(':arrow_down: :regional_indicator_a: :regional_indicator_l: :regional_indicator_t: :arrow_down: \n' + getRandomLine() + '\n:regional_indicator_e: :regional_indicator_n: :regional_indicator_j: :regional_indicator_o: :regional_indicator_y: \n:heart_decoration: :heart: :heart_decoration: :heart: :heart_decoration:');
     	      		client.channels.get('407464458181672961').send('The user ' + msg.author + ' claimed an alt.');
+					
+					// Adds the user to the set so that they can't talk for a minute
+					talkedRecently.add(msg.author.id);
+					setTimeout(() => {
+					  // Removes the user from the set after a minute
+					  talkedRecently.delete(msg.author.id);
+					}, 60000);
+					
 		    }
 	    } else {
 			msg.author.send("Please use this command in the #get-alt channel of our server.");
